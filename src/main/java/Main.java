dc2b6850c10fe98863322619dd040c7ac2efb559
david <dgonzalezl@cifpfbmoll.eu>
2021-04-19 19:23:39 +0200

    Menu opción 1 terminada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,45 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
-        int op1 = 0;
-        int op2 = 0;
-        boolean tornada = true;
-        do {
+        boolean salir= true;// variable salida
+        int opcion;
+        int retorno=0;
+        // variable que va actuar para cada una de las opciones
+        while (salir){
             System.out.println("1- Añadir libro");
             System.out.println("2- Buscar libro por titulo");
             System.out.println("3- Buscar libro por ISBN");
             System.out.println("4- Log in");
             System.out.println("5- Salir");
 
-            op1 = Integer.parseInt(teclado.nextLine());
-            op2 = 0;
+            try {
+                System.out.println("Escribe una de las opciones");
+                /* almaceno en la variable opcion declara en la linea 8 el contenido
+                * que el usuario me haya introducido mediante el Scaner*/
+                opcion = teclado.nextInt();
+                retorno=0;
 
-            switch (op1) {
 
+            switch (opcion) {
                 case 1:
-                    while (op2 != 1) {
-                        System.out.println("1 - Salir");
-                        System.out.println("2 - Añadir otro libro");
 
-                        op2 = Integer.parseInt(teclado.next());
-                        if (op2 == 2) {
-                            Libro.añadirLibro();
-                            op1 = 1;
-                        }
+                    while (retorno != 1) {
+                    Libro.añadirLibro();
+                    System.out.println("1 - Salir");
+                    System.out.println("2 - Añadir otro libro");
+                    retorno=teclado.nextInt();
                     }
                     break;
             }
+            //Seguridad, en caso de poner un String pedir un número
+            } catch (InputMismatchException e){
+                System.out.println("Por favor escriba un número ");
+            }
 
-        } while (op1 != 5);
+        }
     }
 }
